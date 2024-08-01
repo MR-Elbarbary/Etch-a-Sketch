4dcd2bfc935aeb6ace3d078069651aa4c60aad74
MR-Elbarbary <121816435+MR-Elbarbary@users.noreply.github.com>
2024-08-01 11:33:56 +0300

    Create dynamic UI
    Dynamic UI to create the grid default 32x32

---
+++ script.js
@@ -0,0 +1,16 @@
+function buildTheGrid (numberOfBoxes) {
+    let container = document.querySelector(".container")
+    let size = `${100/numberOfBoxes}%`
+    for (let i = 0; i < numberOfBoxes*numberOfBoxes; i++) {
+        let box = document.createElement("div")
+        box.style.width = size
+        box.style.height = size
+        box.setAttribute("class", "block")
+        container.appendChild(box)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    buildTheGrid(16);
+    let resize = document.addEventListener("click", () =>{})
+})
